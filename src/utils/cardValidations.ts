056fa5d20812622c0e14c64cc6713761e6706dc7
osdoonhyun <skw1996@naver.com>
2024-05-07 23:23:26 +0900

    feat: cardValidation 로직 수정, 테스트 코드 작성

--- src/utils/cardValidations.ts
+++ src/utils/cardValidations.ts
@@ -1,26 +1,22 @@
+import { ERROR_MESSAGES } from '@/constants/message';
+import { REGEX } from '@/constants/regex';
 import { FormValues } from '@/type/formType';
 
-const CARD_NUMBER_REGEX = /^\d{4}$/;
-const MONTH_REGEX = /^\d{1,2}$/;
-const YEAR_REGEX = /^\d{2}$/;
-const VERIFICATION_CODE_REGEX = /^\d{3}$/;
-const PIN_NUMBER_REGEX = /^\d{1}$/;
-
 export const CARD_HOLDER_NAME_MAX_LENGTH = 30;
-
-const JANUARY = 1;
-const DECEMBER = 12;
 
 const isValidType = (value: unknown): boolean => {
   return !isNaN(Number(value));
 };
 
 const isValidCardNumber = (cardNumber: string) => {
-  return CARD_NUMBER_REGEX.test(cardNumber);
+  return REGEX.CARD_NUMBER.test(cardNumber);
 };
 
 const isValidMonth = (month: string) => {
-  if (!MONTH_REGEX.test(month)) {
+  const JANUARY = 1;
+  const DECEMBER = 12;
+
+  if (!REGEX.MONTH.test(month)) {
     return false;
   }
 
@@ -28,7 +24,7 @@ const isValidMonth = (month: string) => {
 };
 
 const isValidYear = (year: string) => {
-  return YEAR_REGEX.test(year);
+  return REGEX.YEAR.test(year);
 };
 
 const isValidExpirationDate = (month: string, year: string) => {
@@ -43,11 +39,104 @@ const isValidCardHolderName = (cardHolderName: string) => {
 };
 
 const isValidVerificationCode = (verificationCode: string) => {
-  return VERIFICATION_CODE_REGEX.test(verificationCode);
+  return REGEX.VERIFICATION_CODE.test(verificationCode);
 };
 
 const isValidPinNumber = (pinNumber: string) => {
-  return PIN_NUMBER_REGEX.test(pinNumber);
+  return REGEX.PIN_NUMBER.test(pinNumber);
+};
+
+export const validateCardNumbers = (
+  values: FormValues,
+  errors: Record<string, string> = {}
+) => {
+  if (!isValidType(values.cardNumber1)) {
+    errors.cardNumber1 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidCardNumber(values.cardNumber1)) {
+    errors.cardNumber1 = ERROR_MESSAGES.INVALID_CARD_NUMBER;
+  }
+
+  if (!isValidType(values.cardNumber2)) {
+    errors.cardNumber2 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidCardNumber(values.cardNumber2)) {
+    errors.cardNumber2 = ERROR_MESSAGES.INVALID_CARD_NUMBER;
+  }
+
+  if (!isValidType(values.cardNumber3)) {
+    errors.cardNumber3 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidCardNumber(values.cardNumber3)) {
+    errors.cardNumber3 = ERROR_MESSAGES.INVALID_CARD_NUMBER;
+  }
+
+  if (!isValidType(values.cardNumber4)) {
+    errors.cardNumber4 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidCardNumber(values.cardNumber4)) {
+    errors.cardNumber4 = ERROR_MESSAGES.INVALID_CARD_NUMBER;
+  }
+
+  return errors;
+};
+
+export const validateExpirationDate = (
+  values: FormValues,
+  errors: Record<string, string> = {}
+) => {
+  if (!isValidType(values.expirationMonth)) {
+    errors.expirationMonth = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidMonth(values.expirationMonth)) {
+    errors.expirationMonth = ERROR_MESSAGES.INVALID_MONTH;
+  }
+
+  if (!isValidYear(values.expirationYear)) {
+    errors.expirationYear = ERROR_MESSAGES.INVALID_TYPE;
+  }
+
+  if (!isValidExpirationDate(values.expirationMonth, values.expirationYear)) {
+    errors.expirationYear = ERROR_MESSAGES.EXPIRED_CARD;
+  }
+
+  return errors;
+};
+
+export const validateCardHolderName = (
+  values: FormValues,
+  errors: Record<string, string> = {}
+) => {
+  if (!isValidCardHolderName(values.cardHolderName)) {
+    errors.cardHolderName = ERROR_MESSAGES.INVALID_CARD_HOLDER_NAME;
+  }
+
+  return errors;
+};
+
+export const validateVerificationCode = (
+  values: FormValues,
+  errors: Record<string, string> = {}
+) => {
+  if (!isValidVerificationCode(values.verificationCode)) {
+    errors.verificationCode = ERROR_MESSAGES.INVALID_VERIFICATION_CODE;
+  }
+
+  return errors;
+};
+
+export const validatePinNumbers = (
+  values: FormValues,
+  errors: Record<string, string> = {}
+) => {
+  if (!isValidType(values.pinNumber1)) {
+    errors.pinNumber1 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidPinNumber(values.pinNumber1)) {
+    errors.pinNumber1 = ERROR_MESSAGES.INVALID_PIN_NUMBER;
+  }
+
+  if (!isValidType(values.pinNumber2)) {
+    errors.pinNumber2 = ERROR_MESSAGES.INVALID_TYPE;
+  } else if (!isValidPinNumber(values.pinNumber2)) {
+    errors.pinNumber2 = ERROR_MESSAGES.INVALID_PIN_NUMBER;
+  }
+
+  return errors;
 };
 
 export const cardValidate = (values: FormValues) => {
@@ -64,61 +153,11 @@ export const cardValidate = (values: FormValues) => {
     pinNumber2: '',
   };
 
-  if (!isValidCardNumber(values.cardNumber1)) {
-    errors.cardNumber1 = '카드 번호는 4개의 숫자를 입력해 주세요.';
-  }
-
-  if (!isValidCardNumber(values.cardNumber2)) {
-    errors.cardNumber2 = '카드 번호는 4개의 숫자를 입력해 주세요.';
-  }
-
-  if (!isValidCardNumber(values.cardNumber3)) {
-    errors.cardNumber3 = '카드 번호는 4개의 숫자를 입력해 주세요.';
-  }
-
-  if (!isValidCardNumber(values.cardNumber4)) {
-    errors.cardNumber4 = '카드 번호는 4개의 숫자를 입력해 주세요.';
-  }
-
-  if (!isValidType(values.expirationMonth)) {
-    errors.expirationMonth = '유효하지 않은 형식입니다.';
-  }
-
-  if (!isValidType(values.expirationYear)) {
-    errors.expirationYear = '유효하지 않은 형식입니다.';
-  }
-
-  if (!isValidMonth(values.expirationMonth)) {
-    errors.expirationMonth = '월은 1이상 12이하 숫자여야 합니다.';
-  }
-
-  if (!isValidYear(values.expirationYear)) {
-    errors.expirationYear = '유효한 연도가 아닙니다.';
-  }
-
-  if (!isValidExpirationDate(values.expirationMonth, values.expirationYear)) {
-    errors.expirationYear = '만료일이 지난 카드입니다.';
-  }
-
-  if (!isValidCardHolderName(values.cardHolderName)) {
-    errors.cardHolderName = '이름은 30자 이하로 입력해 주세요.';
-  }
-
-  if (!isValidVerificationCode(values.verificationCode)) {
-    errors.verificationCode = '보안 코드는 숫자만 입력 가능합니다.';
-  }
-
-  if (isValidType(values.pinNumber1 || values.pinNumber2)) {
-    errors.pinNumber1 = '유효하지 않은 형식입니다.';
-  }
-
-  if (!isValidPinNumber(values.pinNumber1)) {
-    errors.pinNumber1 = '카드 비밀번호의 앞 2자리를 입력해 주세요.';
-  }
-
-  if (!isValidPinNumber(values.pinNumber2)) {
-    errors.pinNumber2 = '카드 비밀번호의 앞 2자리를 입력해 주세요.';
-  }
+  validateCardNumbers(values, errors);
+  validateExpirationDate(values, errors);
+  validateCardHolderName(values, errors);
+  validateVerificationCode(values, errors);
+  validatePinNumbers(values, errors);
 
   return errors;
 };
