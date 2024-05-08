b9f73823b2efcc7427f0400c5733d6d5c8c8e7a7
osdoonhyun <skw1996@naver.com>
2024-05-08 23:04:04 +0900

    refactor: GlobalLayout 컴포넌트로 root, app 클래스 분리

--- src/pages/Payments/index.tsx
+++ src/pages/Payments/index.tsx
@@ -7,8 +7,7 @@ import { useStepper } from '@/hooks/useStepper';
 
 export type StepType = typeof CARD_REGISTRATION_STEPS;
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const CARD_REGISTRATION_STEPS = [
+const CARD_REGISTRATION_STEPS = [
   '카드_입력',
   '카드_입력_완료',
   '카드_목록',
@@ -26,27 +25,17 @@ export default function Payments() {
   return (
     <OverlayProvider>
       <CardInfoProvider>
-        <div className='root'>
-          <div className='app'>
-            <Stepper>
-              <Step name={CARD_REGISTRATION_STEPS[0]}>
-                <AddCard
-                  onNext={onChangeStep(1)}
-                  onPrevious={onChangeStep(2)}
-                />
-              </Step>
-              <Step name={CARD_REGISTRATION_STEPS[1]}>
-                <AddCardCompleted onNext={onChangeStep(2)} />
-              </Step>
-              <Step name={CARD_REGISTRATION_STEPS[2]}>
-                <CardList
-                  onNext={onChangeStep(0)}
-                  moveToStep={onChangeStep(1)}
-                />
-              </Step>
-            </Stepper>
-          </div>
-        </div>
+        <Stepper>
+          <Step name={CARD_REGISTRATION_STEPS[0]}>
+            <AddCard onNext={onChangeStep(1)} onPrevious={onChangeStep(2)} />
+          </Step>
+          <Step name={CARD_REGISTRATION_STEPS[1]}>
+            <AddCardCompleted onNext={onChangeStep(2)} />
+          </Step>
+          <Step name={CARD_REGISTRATION_STEPS[2]}>
+            <CardList onNext={onChangeStep(0)} moveToStep={onChangeStep(1)} />
+          </Step>
+        </Stepper>
       </CardInfoProvider>
     </OverlayProvider>
   );
